5ec96acac6f5f2d94ce406ae54cc7e81e5b7f301
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-09 17:39:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 win = window;
 doc = document;
+function elem(type, content, kw) {
+    
+}
 function get_js_version () {
     this.jsv = {
             versions: [ "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0" ],
